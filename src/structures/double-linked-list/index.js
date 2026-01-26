5bf3bbc1816192e96f7b8d49b66e5e9f889f17a1
Alex Cherniakov <rustavellyne@gmail.com>
2026-01-26 23:19:12 +0200

    pop

---
+++ src/structures/double-linked-list/index.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor (value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+
+class DoubleLinkedList {
+  constructor (value = null) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+    if (value !== null) this.push(value);
+  }
+  isEmpty () {
+    return this.length === 0 && !this.head && !this.tail
+  }
+  push(value) {
+    const node = new Node(value);
+    
+    if (this.tail) {
+      this.tail.next = node;
+      node.prev = this.tail;
+    } else {
+      this.head = node;
+    }
+
+    this.tail = node;
+    this.length++;
+
+    return this;
+  }
+  pop () {
+    if (this.isEmpty()) return null;
+    const node = this.tail;
+    this.length--;
+    if (this.length) {
+      this.tail = node.prev;
+      this.tail.next = null;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+    node.prev = null;
+    node.next = null;
+    return node.value;
+  }
+}
+
+export { DoubleLinkedList }
---
+++ src/structures/double-linked-list/index.spec.js
@@ -0,0 +1,40 @@
+import { describe, it, expect, beforeEach, afterEach } from 'bun:test';
+import { DoubleLinkedList } from './index';
+
+describe('Double Linked List', () => {
+  let list;
+  beforeEach(() => {
+    list = new DoubleLinkedList();
+  });
+  afterEach(() => {
+    list = null;
+  });
+  it('creates empty list', () => {
+    expect(list.length).toBe(0);
+    expect(list.isEmpty()).toBe(true);
+  });
+  it('creates list with single value', () => {
+    list = new DoubleLinkedList(99);
+    expect(list.length).toBe(1);
+  });
+  it('pushes values to list and increase length', () => {
+    list.push(10).push(20);
+    expect(list.length).toBe(2);
+    list.push(30);
+    expect(list.length).toBe(3);
+  });
+  it('pops return null on empty list', () => {
+    expect(list.pop()).toBeNull();
+  });
+  it('pops value from list', () => {
+    list.push(10).push(20);
+    expect(list.pop()).toBe(20);
+    expect(list.length).toBe(1);
+  });
+  it('pops correct value from list with one element', () => {
+    list.push(15);
+    expect(list.pop()).toBe(15);
+    expect(list.isEmpty()).toBe(true);
+    expect(list.length).toBe(0);
+  });
+});
